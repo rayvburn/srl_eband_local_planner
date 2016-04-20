580a2acb0ad687c10786bf2dc4669b6a598773da
palmieri <palmieri@cs.uni-freiburg.de>
2016-04-20 11:11:52 +0200

    updating licenses

--- src/srl_eband_trajectory_controller.cpp
+++ src/srl_eband_trajectory_controller.cpp
@@ -32,6 +32,46 @@
 *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 *  POSSIBILITY OF SUCH DAMAGE.
 *
+* Author: Christian Connette
+*********************************************************************/
+/*********************************************************************
+*  srl_eband_local_planner, local planner plugin for move_base based
+*  on the elastic band principle.
+*  License for the srl_eband_local_planner software developed
+*  during the EU FP7 Spencer Projet: new BSD License.
+*
+*  Software License Agreement (BSD License)
+*
+*  Copyright (c) 2015-2016, Luigi Palmieri, University of Freiburg
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of University of Freiburg nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*
 * Author: Luigi Palmieri
 *********************************************************************/
 
@@ -351,7 +391,7 @@ void SrlEBandTrajectoryCtrl::initialize(std::string name, costmap_2d::Costmap2DR
 
         }
         if(plan.size()>0){
-  
+
             pub_local_path_.publish(path_);
             ROS_DEBUG("Plan Published");
         }
@@ -1486,7 +1526,7 @@ bool SrlEBandTrajectoryCtrl::getTwistDifferentialDrive(geometry_msgs::Twist& twi
                  robot_cmd.angular.z = rotation_sign * max_rotational_velocity_turning_on_spot_;
             }
 
-            ROS_WARN("Performing in place for the initial turn flipped: %f", bubble_diff.angular.z, robot_cmd.angular.z);
+            ROS_WARN("Performing in place for the initial turn flipped: diff %f  and w %f", bubble_diff.angular.z, robot_cmd.angular.z);
             command_provided = true;
             twist_cmd = robot_cmd;
             return true;
